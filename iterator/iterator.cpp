c76fc9ef9ac549e1165597e178cc0a6cb16885c6
Keith A. Lewis <kal@kalx.net>
2015-05-11 17:01:41 -0400

    new files

---
+++ iterator/iterator.cpp
@@ -0,0 +1,61 @@
+// iterator.cpp
+#include <cassert>
+#include "iterator.h"
+
+using namespace iterator;
+
+template<class I>
+inline typename I::value_type sum(I i, size_t n = 1000)
+{
+	typename I::value_type s = 0;
+
+	while (n-- && (*i == 0 || 1 + *i != 1))
+		s += *i++;
+
+	return s;
+}
+
+int main() 
+{
+	try {
+		auto n_ = factorial<double>{};
+		auto _1 = constant<double>(1);
+
+		auto s = _1/n_;
+		assert (*s == 1);
+		assert (*++s == 1);
+		assert (*++s == 1/2.);
+		assert (*++s == 1/6.);
+
+		auto e = sum(_1/n_);
+		double f = e - exp(1);
+		assert (fabs(f) <= 2*std::numeric_limits<double>::epsilon());
+		f = sum(constant<>(1)/factorial<>{});;
+
+		double x = 2;
+		auto expx = sum(iterator::pow<double>(x)/factorial<>{});
+		f = exp(2.) - expx;
+
+		test_apply();
+		test_concatenate();
+		test_constant();
+		test_enumerator();
+		test_expr();
+		test_factorial();
+		test_input();
+		test_iota();
+		test_pair();
+		test_pow();
+		test_skip();
+		/*
+//		test_sequence();
+		*/
+
+/*		
+*/	}
+	catch (...) {
+		return -1;
+	}
+
+	return 0;
+}
---
+++ iterator/pow.h
@@ -0,0 +1,57 @@
+// pow.h - powers of a number
+#pragma once
+#include "input.h"
+
+namespace iterator {
+
+	template<class T>
+	class pow : public input_base<T*, T> {
+		T t, t_;
+	public:
+		pow(T t = 0)
+			: t(t), t_(1)
+		{ }
+		pow(const pow&) = default;
+		pow& operator=(const pow&) = default;
+		pow(pow&&) = default;
+		pow& operator=(pow&&) = default;
+		~pow()
+		{ }
+
+		T operator*() const
+		{
+			return t_;
+		}
+		pow& operator++()
+		{
+			t_ *= t;
+
+			return *this;
+		}
+		pow operator++(int)
+		{
+			pow p(*this);
+
+			operator++();
+
+			return p;
+		}
+	};
+
+} // iterator
+
+#ifdef _DEBUG
+#include <cassert>
+
+inline void test_pow()
+{
+	iterator::pow<int> p(2);
+
+	assert (*p == 1);
+	assert (*++p == 2);
+	p++;
+	assert (*p == 4);
+	assert (*++p == 8);
+}
+
+#endif // _DEBUG
